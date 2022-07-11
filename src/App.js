dca7f11a55c2ebae14746b25542d4db10e5c9a26
Sajeeb77725 <mandal15-4045@diu.edu.bd>
2022-07-11 11:29:51 +0600

    toggol button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  getAuth,
+} from "firebase/auth";
 import app from "./firebase.init";
 import Form from "react-bootstrap/Form";
 import { Button } from "react-bootstrap";
@@ -11,6 +15,8 @@ const auth = getAuth(app);
 function App() {
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
+  const [error, setError] = useState("");
+  const [registered, setRegistered] = useState(false);
 
   const handleEmailFeild = (event) => {
     setEmail(event.target.value);
@@ -20,22 +26,41 @@ function App() {
     setPass(event.target.value);
   };
 
+  const handleRegistered = (event) => {
+    setRegistered(event.target.checked);
+  };
+
   const handleFormSubmit = (event) => {
-    createUserWithEmailAndPassword(auth, email, pass)
-      .then((result) => {
-        const user = result.user;
-        console.log(user);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    if (registered) {
+      signInWithEmailAndPassword(auth, email, pass)
+        .then((result) => {
+          console.log(result.user);
+        })
+        .catch((error) => {
+          setError(error.message);
+        });
+    } else {
+      createUserWithEmailAndPassword(auth, email, pass)
+        .then((result) => {
+          const user = result.user;
+          console.log(user);
+          setEmail("");
+          setPass("");
+        })
+        .catch((error) => {
+          console.error(error);
+          setError(error.message);
+        });
+    }
     event.preventDefault();
   };
 
   return (
     <div>
       <div className="register w-50 mx-auto mt-5">
-        <h1 className="text-primary">Please Register!</h1>
+        <h1 className="text-primary">
+          Please {registered ? "Log In" : "Register!"}
+        </h1>
         <Form onSubmit={handleFormSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
@@ -43,22 +68,31 @@ function App() {
               onBlur={handleEmailFeild}
               type="email"
               placeholder="Enter email"
+              required
             />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
           </Form.Group>
-
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
             <Form.Control
               onBlur={handlePassFeild}
               type="password"
               placeholder="Password"
+              required
+            />
+          </Form.Group>
+          <p>{error}</p>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check
+              onChange={handleRegistered}
+              type="checkbox"
+              label="Already registered?"
             />
           </Form.Group>
           <Button variant="primary" type="submit">
-            Submit
+            {registered ? "Log In" : "Register"}
           </Button>
         </Form>
       </div>
